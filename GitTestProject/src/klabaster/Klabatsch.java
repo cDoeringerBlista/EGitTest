5082a88ce72cef0dd34d4382dc387843f8f6d360
Christian Doeringer <c.doeringer@blista.de>
2017-02-22 10:04:02 +0100

    Commit 4

--- GitTestProject/src/klabaster/Klabatsch.java
+++ GitTestProject/src/klabaster/Klabatsch.java
@@ -3,7 +3,7 @@ package klabaster;
 public class Klabatsch {
 	public static void main (String[] args) {
 		// Hurr Durr
-		System.out.println("Hurr Durr");
+		System.out.println("Klabaster und Klabatsch");
 		Desk desk = new Desk();
 		System.out.println(desk.getDescription());
 	} // main()
