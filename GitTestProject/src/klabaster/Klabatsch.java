3d52ca53fbcfc6a0aadb7ace717b05ba0ccf21d6
Christian Doeringer <c.doeringer@blista.de>
2017-02-22 09:45:44 +0100

    first Commit

---
+++ GitTestProject/src/klabaster/Klabatsch.java
@@ -0,0 +1,8 @@
+package klabaster;
+
+public class Klabatsch {
+	public static void main (String[] args) {
+		// Klabaster und Klabatsch
+		System.out.println("Klabaster und Klabatsch");
+	} // main()
+} // class
