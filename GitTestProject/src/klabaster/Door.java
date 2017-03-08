ebcf52eb3826d43482c3b7100563b77e5bec3b6a
Christian Doeringer <c.doeringer@blista.de>
2017-03-08 11:50:43 +0100

    erste Aenderung für GitHub 2ter Versuch

--- GitTestProject/src/klabaster/Door.java
+++ GitTestProject/src/klabaster/Door.java
@@ -3,9 +3,6 @@ package klabaster;
 public class Door {
 	public String getDescription() { 
 		return "eine Tür";
-		// Hurr Durr
-		// Hurr Durr 2
-		// hUrr Durr 13
-		// Hurr Durr 2
+		// erste Aenderung für GitHub
 	} // getDescription()
 } // class 
