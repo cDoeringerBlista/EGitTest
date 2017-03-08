5bf16c5640dcba666f9cf0e9237ed48a96eb0cc2
Christian Doeringer <c.doeringer@blista.de>
2017-03-08 10:10:17 +0100

    Commit blusdf

--- GitTestProject/src/klabaster/Door.java
+++ GitTestProject/src/klabaster/Door.java
@@ -5,7 +5,10 @@ public class Door {
 		return "eine Tür";
 		// Hurr Durr
 		// Hurr Durr 2
+<<<<<<< Upstream, based on branch_c
 		// hUrr Durr 13
 		// Hurr Durr 4
+=======
+>>>>>>> 3de96ea Commit blusdf
 	} // getDescription()
 } // class 
