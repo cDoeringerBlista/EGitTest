18500ef011628c31a36746cb4ff52ed54162953a
Christian Doeringer <c.doeringer@blista.de>
2017-03-17 10:31:23 +0100

    klabaster klabatsch hurr

--- GitTestProject/src/klabaster/Door.java
+++ GitTestProject/src/klabaster/Door.java
@@ -2,7 +2,7 @@ package klabaster;
 
 public class Door {
 	public String getDescription() { 
-		return "klabatsch";
+		return "klabaster klabatsch hurr";
 		// Ändern und von Eclipse zu GitHub
 		// Änderung xyz
 	} // getDescription()
